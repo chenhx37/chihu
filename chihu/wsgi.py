4c3f4b8df8915303540880250240c6acc25e41de
Chenhx <465462686@qq.com>
2016-02-19 20:13:21 +0800

    django 1.9.2->1.8.3

--- chihu/wsgi.py
+++ chihu/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+"""
 import os
-
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chihu.settings")
+application = get_wsgi_application()
+"""
 
+import os
+import sys
+from django.core.wsgi import get_wsgi_application
+root = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(root, '..', 'site-packages'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chihu.settings")
 application = get_wsgi_application()
